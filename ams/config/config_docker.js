c2c80c9250c8ddc91cc643c9a0a5d2e392b6c704
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-07-11 10:48:50 +0200

    updated dockerfile and config for ams

--- ams/config/config_docker.js
+++ ams/config/config_docker.js
@@ -1,53 +1,24 @@
-/**
- * Created by beuttlerma on 12.06.17.
- */
+var self = {};
 
-String.prototype.format = function () {
-    var args = [].slice.call(arguments);
-    return this.replace(/(\{\d+\})/g, function (a) {
-        return args[+(a.substr(1, a.length - 2)) || 0];
-    });
+self.HOST_SETTINGS = {
+    MARKETPLACE_CORE: {
+        PROTOCOL: 'http',
+        HOST: 'core',
+        PORT: 3002
+    },
+    OAUTH_SERVER: {
+        PROTOCOL: 'http',
+        HOST: 'auth',
+        PORT: 3006
+    },
 };
 
-var self = {
-
+// ---- CONFIGURATION EXPORT ----
+self.OAUTH_CREDENTIALS = {
+    CLIENT_ID: 'adb4c297-5f53-4332-88ff-07398ee44b6e',
+    CLIENT_SECRET: 'IsSecret'
 };
 
 self.LOG_LEVEL = 'debug';
-
-var username = 'docker';
-var password = 'docker';
-var host = 'auth-db';
-var port = '5432';
-var database = 'oauthdb';
-
-
-self.DB_CONNECTION_STRING = 'postgres://{0}:{1}@{2}:{3}/{4}'.format(username, password, host, port, database);
-self.OAUTH_CREDENTIALS = {
-    CLIENT_ID: 'adb4c297-45bd-437e-ac90-9179eea41744',
-    CLIENT_SECRET: 'IsSecret',
-    CALLBACK_URL: 'http://localhost:3004/auth/iuno/callback'
-};
-
-self.OAUTH_PROVIDER = {
-    facebookAuth: {
-        'clientID': '', // your App ID
-        'clientSecret': '', // your App Secret
-        'callbackURL': '',
-        'profileURL': '',
-        'profileFields': ['id', 'name', 'photos']
-    },
-    twitterAuth: {
-        'consumerKey': '',
-        'consumerSecret': '',
-        'callbackURL': ''
-    },
-    googleAuth: {
-        'clientID': '',
-        'clientSecret': '',
-        'callbackURL': ''
-    }
-};
-
 
 module.exports = self;
